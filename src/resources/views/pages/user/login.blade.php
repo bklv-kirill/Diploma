bd205b71161be2efed27a919f89f0f32e5171848
bklv.kirill <bklv.kirill@yandex.ru>
2024-04-04 21:44:23 +0300

    oh oh oh diploma work

--- src/resources/views/pages/user/login.blade.php
+++ src/resources/views/pages/user/login.blade.php
@@ -5,12 +5,17 @@
             <x-auth-register-input name="email" type="email" placeholder="Введите email" required/>
             <x-auth-register-input name="password" type="password" placeholder="Введите пароль" required/>
 
+            <div class="remember-me">
+                <input type="checkbox" name="remember_me" id="remember">
+                <label for="remember"><h3>Запомнить меня</h3></label>
+            </div>
+
             <div class="input-box button">
                 <input type="submit" value="Войти">
             </div>
 
             <div class="text">
-                <h3>У вас еще нет аккаунте? <a href="{{ route('user.register') }}">Зарегестрироваться</a></h3>
+                <h3>У вас еще нет аккаунта? <a href="{{ route('user.register') }}">Зарегестрироваться</a></h3>
             </div>
             <div class="text">
                 <h3>Вернуться на главную <a href="{{ route('main') }}">страницу</a></h3>
