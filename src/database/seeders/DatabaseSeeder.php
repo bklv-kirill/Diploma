bc408ec7350c98b25097fca0f5219259b08036c0
Bakalov Kirill <bklv.kirill@yandex.ru>
2024-05-12 01:09:51 +0500

    - Удалил ненужные файлы.

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -2,8 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Chart;
-use App\Models\Employment;
 use Illuminate\Database\Seeder;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
@@ -12,38 +10,5 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        Employment::factory()->create([
-            'name' => 'Полная занятость',
-            'slug' => 'full-employment',
-        ]);
-        Employment::factory()->create([
-            'name' => 'Частичная занятость',
-            'slug' => 'partly',
-        ]);
-        Employment::factory()->create([
-            'name' => 'Стажировка',
-            'slug' => 'internship',
-        ]);
-
-        Chart::factory()->create([
-            'name' => 'Полный день',
-            'slug' => 'full-day',
-        ]);
-        Chart::factory()->create([
-            'name' => 'Сменный график',
-            'slug' => 'replaceable',
-        ]);
-        Chart::factory()->create([
-            'name' => 'Гибкий график',
-            'slug' => 'flexible',
-        ]);
-        Chart::factory()->create([
-            'name' => 'Удаленная работа',
-            'slug' => 'remotely',
-        ]);
-        Chart::factory()->create([
-            'name' => 'Вахтовый метод',
-            'slug' => 'watch',
-        ]);
     }
 }
