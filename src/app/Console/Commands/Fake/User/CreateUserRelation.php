bbc0d9da7e607ebbddc059afa74503f9d95c4ad1
Bakalov Kirill <bklv.kirill@yandex.ru>
2024-05-25 02:08:02 +0500

    - Добавил команду для создания отношений у пользователя. - Дополнил SQL дамп.

---
+++ src/app/Console/Commands/Fake/User/CreateUserRelation.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Console\Commands\Fake\User;
+
+use App\Models\Collage;
+use App\Models\Hard;
+use App\Models\Soft;
+use App\Models\University;
+use App\Models\User;
+use Illuminate\Console\Command;
+
+class CreateUserRelation extends Command
+{
+
+    protected $signature = 'user-create:relations';
+
+    protected $description = 'Command for creating user relations';
+
+    public function handle()
+    {
+        $users = User::query()->applicants()->get();
+
+        $users->each(function (User $user) {
+            if ((bool)rand(0, 1)) {
+                $university = University::query()->inRandomOrder()->first();
+
+                $user->universities()->attach($university->id);
+            }
+
+            if ((bool)rand(0, 1)) {
+                $collage = Collage::query()->inRandomOrder()->first();
+
+                $user->collages()->attach($collage->id);
+            }
+
+            if ((bool)rand(0, 1)) {
+                $soft = Soft::query()->inRandomOrder()->first();
+
+                $user->softs()->attach($soft->id);
+            }
+
+            if ((bool)rand(0, 1)) {
+                $hard = Hard::query()->inRandomOrder()->first();
+
+                $user->hards()->attach($hard->id);
+            }
+        });
+
+        dd('User relations have been created');
+    }
+
+}
--- src/app/Console/Commands/Fake/User/LoadUserAvatar.php
+++ src/app/Console/Commands/Fake/User/LoadUserAvatar.php
@@ -12,6 +12,7 @@ use Illuminate\Support\Facades\Storage;
 
 class LoadUserAvatar extends Command
 {
+
     protected $signature = 'user-avatar:load';
 
     protected $description = 'Command for load user avatar';
@@ -19,17 +20,23 @@ class LoadUserAvatar extends Command
     public function handle(LoadUserAvatarIfExistsAction $loadUserAvatarIfExists)
     {
         $avatarsPaths = Storage::disk('images')->allFiles('fake/user');
-        $users = User::applicants()->get();
+        $users        = User::query()->applicants()->get();
 
-        $users->each(function (User $user) use ($avatarsPaths, $loadUserAvatarIfExists) {
+        $users->each(function (User $user) use (
+            $avatarsPaths,
+            $loadUserAvatarIfExists
+        ) {
             $avatarPath = Arr::random($avatarsPaths);
-            $avatarName = File::name(Arr::random($avatarsPaths)) . '.' . File::extension(Arr::random($avatarsPaths));
+            $avatarName = File::name(Arr::random($avatarsPaths)).'.'
+                .File::extension(Arr::random($avatarsPaths));
 
-            $uploadAvatar = new UploadedFile(Storage::disk('images')->path($avatarPath), $avatarName);
+            $uploadAvatar = new UploadedFile(Storage::disk('images')
+                ->path($avatarPath), $avatarName);
 
             $loadUserAvatarIfExists($user, $uploadAvatar);
         });
 
         dd('User avatar have been loaded');
     }
+
 }
